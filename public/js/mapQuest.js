bffa637c723a63c1057b162f33f24a7cacceabf2
Geon Yoo <46584775+jasongeon123@users.noreply.github.com>
2021-02-25 14:39:34 -0800

    thirdpartyAPI lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -3,12 +3,12 @@ function initMap() {
 
     // 'map' refers to a <div> element with the ID map
     var map = L.mapquest.map('map', {
-        center: [32.88, -117.236],
+        center: [32.878637429420166, -117.23584844944718],
         layers: L.mapquest.tileLayer('map'),
         zoom: 12,
         zoomControl: false
     });
 
     // Add a marker to the map
-    L.marker([32.88, -117.236]).addTo(map);
+    L.marker([32.878637429420166, -117.235848449447186]).addTo(map);
 }
